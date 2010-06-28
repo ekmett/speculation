acfc8b473bbab02d24aff0466cb161c44ba3edcb
ekmett <ekmett@gmail.com>
2010-06-27 22:50:45 -0700

    benchmark skeleton

--- Benchmark.hs
+++ Benchmark.hs
@@ -1,5 +1,15 @@
 module Main where
 
+import Criterion.Main
+import Criterion
+
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
 main :: IO ()
-main = do
-    return ()
+main = defaultMain 
+    [ bench "fib 1" $ nf fib 1
+    , bench "fib 2" $ nf fib 2
+    ]
